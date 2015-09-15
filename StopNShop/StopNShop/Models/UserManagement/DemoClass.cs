a4846e16c307a4d410709cbb47a03b38637fec5d
msingh1 <msingh1@visa.com>
2015-09-15 12:54:32 +0530

    +Init

---
+++ StopNShop/StopNShop/Models/UserManagement/DemoClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StopNShop.Models.UserManagement
+{
+    public class DemoClass
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+    }
+}
---
+++ StopNShop_1/StopNShop_1/Models/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StopNShop_1.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(15,MinimumLength=6,ErrorMessage="Minimum 6 Maximum 15 character allowed")]
+        public string Password { get; set; }
+    }
+}
